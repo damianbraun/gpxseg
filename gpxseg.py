04411aed216a1f71a3d2086228eaee9aaf9cedd6
Damian Braun <brunek5252@gmail.com>
2014-06-08 18:30:36 +0200

    files added

---
+++ gpxseg.py
@@ -0,0 +1,198 @@
+#! /usr/bin/env python
+"""gpxseg
+
+Usage:
+    gpxseg -c -s <source_folder> -t <target_folder>
+    gpxseg -m -s <source_folder> -t <target_folder>
+
+Options:
+    -h --help       Show this screen.
+    --version       Show version.
+    -c              Copy mode.
+    -m              Move mode.
+    -s --source     Set source folder.
+    -t --target     Set target folder.
+"""
+from __future__ import print_function
+from docopt import docopt
+args = docopt(__doc__)
+
+from pprint import pprint
+from xml.dom import minidom
+import datetime
+import logging
+import os
+import shutil
+import sys
+
+from nominatim import NominatimReverse
+
+logger = logging.getLogger()
+watchpaths = []
+watchpaths.append(args['<source_folder>'])
+targetpath = args['<target_folder>']
+
+def find_files_in_folder(folderpaths=watchpaths,
+                         extentions=['gpx']):
+    """
+    Return list of paths to files with extentions from second argument which
+    are stored in folders from first argument.
+    Both arguments should be lists.
+    """
+    files_list = []
+    for x in folderpaths:
+        x = os.path.expanduser(x)
+        for root, dirs, files in os.walk(x):
+            for file in files:
+                for extention in extentions:
+                    if extention in file[-len(extention):]:
+                        """
+                        file[-len(extention):] beacouse it shuld work only if
+                        end of file name is matching, TODO: i can try using
+                        endswith method of str
+                        """
+                        files_list.append(os.path.join(root, file))
+    return files_list
+
+class Coordinate(object):
+    """docstring for Coordinate"""
+    def __init__(self, lat, lon, dt):
+        self.lat, self.lon = lat, lon
+        self.dt = dt
+        self.address = ''
+
+    def __repr__(self):
+        if self.address:
+            return '''Coordinate instance:\nlat: %s\nlon: %s\ndt: %s
+address:%s''' % (self.lat, self.lon, self.dt, self.address)
+        else:
+            return ''''Coordinate instance:\nlat: %s\nlon: %s
+dt: %s''' % (self.lat, self.lon, self.dt)
+
+    def fetchaddress(self):
+        """docstring for fetchaddress"""
+
+        nomrev = NominatimReverse()
+        self.osm = nomrev.query(self.lat, self.lon)
+        
+        adr = self.osm['address']
+
+        if not 'road' in adr:
+            adr[u'road'] = u''
+        if not 'path' in adr:
+            adr[u'path'] = u''
+
+        if not 'house_number' in adr:
+            adr[u'house_number'] = u''
+
+        if not 'city_district' in adr:
+            adr[u'city_district'] = u''
+        if not 'suburb' in adr:
+            adr[u'suburb'] = u''
+
+        if not 'city' in adr:
+            adr[u'city'] = u''
+        if not 'town' in adr:
+            adr[u'town'] = u''
+        if not 'village' in adr:
+            adr[u'village'] = u''
+
+        if adr['house_number'] and adr['road']:
+            adr[u'road_house_number'] = adr['road'] + u' ' + adr['house_number'] + u', '
+        elif adr['road']:
+            adr[u'road_house_number'] = adr['road'] + u', '
+        elif adr['path']:
+            adr[u'road_house_number'] = adr['path'] + u', '
+        else:
+            adr[u'road_house_number'] = u''
+
+        if adr['city_district'] and adr['suburb']:
+            adr[u'district_suburb'] = adr['suburb'] + u', '
+        elif adr['city_district']:
+            adr[u'district_suburb'] = adr['city_district'] + u', '
+        elif adr['suburb']:
+            adr[u'district_suburb'] = adr['suburb'] + u', '
+        else:
+            adr[u'district_suburb'] = u''
+
+        sn = u'{road_house_number}{district_suburb}{city}{town}{village}'.format(**adr)
+        self.shortaddress = sn.strip()
+
+
+class Gpx(object):
+    """docstring for Gpx"""
+    def __init__(self, filepath):
+        self.filepath = str(filepath)
+
+    def load(self):
+        """docstring for load"""
+        self.DOMTree = minidom.parse(self.filepath)
+        trkptlist = self.DOMTree.getElementsByTagName('trkpt')
+        self.ITEMS = []
+        for item in trkptlist:
+            if sys.version_info.major == 3:
+                time = str(item.getElementsByTagName('time')[0].firstChild.data.encode('utf-8'), encoding='utf-8')
+            else: 
+                time = str(item.getElementsByTagName('time')[0].firstChild.data.encode('utf-8'))
+            try:
+                dt = datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%S.%fZ')
+            except:
+                dt = datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%SZ')
+            coordinate = Coordinate(item.attributes['lat'].value,
+                                    item.attributes['lon'].value,
+                                    dt)
+            self.ITEMS.append(coordinate)
+
+    def namegen(self):
+        """docstring for namegen"""
+        def z(x):
+            if type(x) == 'str':
+                if int(x) < 10:
+                    return '0' + x
+                else:
+                    return x
+            else:
+                if x < 10:
+                    return '0' + str(x)
+                else:
+                    return str(x)
+        first = self.ITEMS[0]
+        first.fetchaddress()
+        last = self.ITEMS[-1]
+        self.newname = '%s-%s-%s %s.%s %s' % (first.dt.year,
+        z(first.dt.month), z(first.dt.day), z(first.dt.hour),
+                z(first.dt.minute), first.shortaddress)
+        self.newname = self.newname.replace('/','.')
+
+    def copyfile(self):
+        """docstring for copyfile"""
+        target = os.path.join(os.path.expanduser(targetpath), self.newname + '.gpx')
+        if sys.version_info.major == 2:
+            target = target.encode('utf-8', 'ignore')
+        if self.newname:
+            shutil.copyfile(self.filepath, target)
+            print('%s\ncopied to\n%s' % (self.filepath, target))
+
+    def movefile(self):
+        """docstring for movefile"""
+        target = os.path.join(os.path.expanduser(targetpath), self.newname + '.gpx')
+        if sys.version_info.major == 2:
+            target = target.encode('utf-8', 'ignore')
+        if self.newname:
+            shutil.move(self.filepath, target)
+            print('%s\nmoved to\n%s' % (self.filepath, target))
+
+def main():
+    fileslist = find_files_in_folder()
+    for f in fileslist:
+        gpx = Gpx(f)
+        gpx.load()
+        gpx.namegen()
+        if args['-c']:
+            gpx.copyfile()
+        if args['-m']:
+            gpx.movefile()
+        print('')
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+        name='gpxseg',
+        version='0.1',
+        author='Damian Braun',
+        author_email='brunek5252@gmail.com',
+        packages=[],
+        py_modules=['gpxseg'],
+        entry_points={
+            'console_scripts': ['gpxseg=gpxseg:main']
+            },
+        install_requires=[
+            'docopt'
+            ],
+        classifiers=[
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7'
+            ]
+)
