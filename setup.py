ed4bce9a182f4714db2e3f6f15c85a24e31dd76b
Damian Braun <brunek5252@gmail.com>
2014-06-11 22:22:09 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 
 setup(
-        name='gpxseg',
-        version='0.1',
-        author='Damian Braun',
-        author_email='brunek5252@gmail.com',
-        packages=['nominatim'],
-        py_modules=['gpxseg'],
-        entry_points={
-            'console_scripts': ['gpxseg=gpxseg:main']
-            },
-        install_requires=[
-            'docopt'
-            ],
-        classifiers=[
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7'
-            ]
+    name='gpxseg',
+    version='0.1',
+    author='Damian Braun',
+    author_email='brunek5252@gmail.com',
+    py_modules=['gpxseg'],
+    entry_points={
+        'console_scripts': ['gpxseg=gpxseg:main']
+        },
+    install_requires=[
+        'docopt',
+        'nominatim'
+        ],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Console'
+        ]
 )
